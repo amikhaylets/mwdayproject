3236882c75be18961e541a46fba2a1627cf40538
Aleksandr <a.mikhaylets@yahoo.com>
2015-03-31 00:12:47 +0600

    Add background page

--- src/mwday/views.py
+++ src/mwday/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# def home2(request):
-# 	context = {}
-# 	template = "home2.html"
-# 	return render(request, template, context)
+def testhome(request):
+	context = {}
+	template = "donotuse.html"
+	return render(request, template, context)
