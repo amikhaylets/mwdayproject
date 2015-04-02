9f1fe1d0a1ad8db5f3b94a6ff1a56c67c64e08a1
Aleksandr <a.mikhaylets@yahoo.com>
2015-04-03 00:25:01 +0600

    Add background picture

--- src/mwday/urls.py
+++ src/mwday/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'joins.views.home', name='home'),
-    url(r'^testhome$', 'mwday.views.testhome', name='testhome'),
+    # url(r'^testhome$', 'mwday.views.testhome', name='testhome'),
     url(r'^(?P<ref_id>.*)$', 'joins.views.share', name='share'),
     # url(r'^home2/$', 'mwday.views.home2', name='home'),
     # url(r'^blog/', include('blog.urls')),
